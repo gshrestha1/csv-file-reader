4297caccf0cead8b6abdd9e24fdb8179543473e0
Gopal Shrestha <gshrestha1@gmail.com>
2020-05-23 14:40:14 -0400

    Formatted code

--- src/main/java/CSVFileReaderApplication.java
+++ src/main/java/CSVFileReaderApplication.java
@@ -2,10 +2,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CSVFileReaderApplication {
+public
+class CSVFileReaderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CSVFileReaderApplication.class, args);
+        SpringApplication.run(CSVFileReaderApplication.class,args);
     }
 }
 
